0571ff252928c4030d7f5b2713d37a1b05c7b77c
bruno <bruno.crosier@gmail.com>
2019-06-20 00:38:50 +0100

    jasdgja

--- frontend/.next/server/static/development/pages/_app.js
+++ frontend/.next/server/static/development/pages/_app.js
@@ -1696,15 +1696,17 @@ var Supreme = styled_components__WEBPACK_IMPORTED_MODULE_0___default.a.h3.withCo
 /*!*******************!*\
   !*** ./config.js ***!
   \*******************/
-/*! exports provided: endpoint, perPage */
+/*! exports provided: endpoint, prodEndpoint, perPage */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "endpoint", function() { return endpoint; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "prodEndpoint", function() { return prodEndpoint; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "perPage", function() { return perPage; });
 // This is client side config only - don't put anything in here that shouldn't be public!
 var endpoint = "http://localhost:4444";
+var prodEndpoint = "https://sickfits-yoga-prod-bru.herokuapp.com/";
 var perPage = 4;
 
 /***/ }),
@@ -1777,9 +1779,9 @@ function createClient(_ref) {
     uri:  true ? _config__WEBPACK_IMPORTED_MODULE_2__["endpoint"] : undefined,
     request: function request(operation) {
       operation.setContext({
-        fetchOptions: {
-          credentials: "include"
-        },
+        // fetchOptions: {
+        //   credentials: "include"
+        // },
         headers: headers
       });
     },
--- frontend/.next/static/development/pages/_app.js
+++ frontend/.next/static/development/pages/_app.js
@@ -1583,15 +1583,17 @@ var Supreme = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].h3.withC
 /*!*******************!*\
   !*** ./config.js ***!
   \*******************/
-/*! exports provided: endpoint, perPage */
+/*! exports provided: endpoint, prodEndpoint, perPage */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "endpoint", function() { return endpoint; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "prodEndpoint", function() { return prodEndpoint; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "perPage", function() { return perPage; });
 // This is client side config only - don't put anything in here that shouldn't be public!
 var endpoint = "http://localhost:4444";
+var prodEndpoint = "https://sickfits-yoga-prod-bru.herokuapp.com/";
 var perPage = 4;
 
 /***/ }),
@@ -1663,9 +1665,9 @@ function createClient(_ref) {
     uri:  true ? _config__WEBPACK_IMPORTED_MODULE_2__["endpoint"] : undefined,
     request: function request(operation) {
       operation.setContext({
-        fetchOptions: {
-          credentials: "include"
-        },
+        // fetchOptions: {
+        //   credentials: "include"
+        // },
         headers: headers
       });
     },
