41aa67e99b30b0b0c98cb322a311c143d8116a30
Ethan Smith <ethanfsmith@gmail.com>
2019-08-23 13:47:34 -0700

    remove secrets from now.json. new gitignore file

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2110b67acb3e71602c7f";
+/******/ 	var hotCurrentHash = "bc4b9fda6c9c6664760c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
